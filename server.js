68d27f0d78639058a12dc65cfb5ebd85dc1766f0
wandreuspereira <wandreus.pereira@original.io>
2021-05-17 21:55:37 -0300

    add chat

--- server.js
+++ server.js
@@ -12,16 +12,28 @@ const io = require("socket.io")(server, {
 });
 
 server.listen("8080", () => console.log("Server run"));
-let count = [];
 let settings = {};
+let allmessages = [];
 
 io.on("connection", (socket) => {
-  count.push(socket?.id);
-  socket.on("sendSku", (sku) => {
-    settings = sku;
-    console.log("sku :>> ", sku);
-    socket.broadcast.emit("receivedSku", sku);
+  // default settings initial
+  socket.broadcast.emit("init", { settings, allmessages });
+
+  // Chat
+  socket.on("logeed", (name) => {
+    const message = { Textmessages: "Entrou", name, entry: true };
+    allmessages.push(message);
+    socket.broadcast.emit("updateMessages", message);
   });
 
-  socket.broadcast.emit("init", settings);
+  socket.on("SendMessage", (el) => {
+    allmessages.push(el);
+    socket.broadcast.emit("updateMessages", el);
+  });
+
+  // Product
+  socket.on("sendSku", (sku) => {
+    settings = sku;
+    socket.broadcast.emit("receivedSku", sku);
+  });
 });

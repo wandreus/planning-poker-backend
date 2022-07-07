51814bcbaefce26d4f079d03d773a0ae36b85f49
Wandreus Pereira <33557525+wandreus@users.noreply.github.com>
2022-07-07 17:54:01 -0300

    feat: finish all basics features

--- index.js
+++ index.js
@@ -12,19 +12,18 @@ server.listen("8080", () => console.log("Server run"));
 
 let voting = [];
 let users = [];
+let showVoting = false;
 
 io.on("connection", (socket) => {
-  socket.emit("init", { voting, users });
+  socket.emit("init", { voting, users, showVoting });
 
   socket.on("logeed", (user) => {
     const newUser = users.filter((u) => u?.user?.email !== user?.user?.email);
 
     const newList = [...newUser, user].sort((a, b) => {
-      return a?.user?.name < b?.user?.name
-        ? -1
-        : a?.user?.name > b?.user?.name
-        ? 1
-        : 0;
+      const aName = a?.user?.name;
+      const bName = b?.user?.name;
+      return aName < bName ? -1 : aName > bName ? 1 : 0;
     });
 
     if (user) users = newList;
@@ -35,6 +34,16 @@ io.on("connection", (socket) => {
   socket.on("voting", (vot) => {
     const newVoting = voting.filter((u) => u?.user?.email !== vot?.user?.email);
     if (vot) voting = [...newVoting, vot];
+    io.emit("AllVoting", { voting });
+  });
+
+  socket.on("show", (show) => {
+    showVoting = show;
+    io.emit("showVoting", { showVoting });
+  });
+
+  socket.on("reset", () => {
+    voting = [];
     io.emit("AllVoting", { voting });
   });
 });
